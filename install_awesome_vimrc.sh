7bcfb3ed01c9d1809caa21de63557e3adc09d423
Prathamesh <prathamesh.netake@gmail.com>
2018-04-17 20:51:33 +0530

    refactor some rules

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+cd ~/vimdotfile
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/vimdotfile
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/vimdotfile/vimrcs/basic.vim
+source ~/vimdotfile/vimrcs/filetypes.vim
+source ~/vimdotfile/vimrcs/plugins_config.vim
+source ~/vimdotfile/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/vimdotfile/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
